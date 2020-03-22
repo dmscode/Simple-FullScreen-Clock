295ca713132839b76c6c7fa7657ce1eb497bc100
dmscode <i@zji.me>
2020-03-22 16:55:13 +0800

    CACHE_NAME

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@ self.addEventListener('fetch', function(e) {
     caches.match(e.request).then(function(r) {
       console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
+        return caches.open(CACHE_NAME).then(function(cache) {
           console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
@@ -31,7 +31,7 @@ self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keyList) {
           return Promise.all(keyList.map(function(key) {
-        if(cacheName.indexOf(key) === -1) {
+        if(CACHE_NAME.indexOf(key) === -1) {
           return caches.delete(key);
         }
       }));
