fe493a0d460f5df67794354155cc52cfc583bc75
dmscode <i@zji.me>
2020-03-22 16:54:08 +0800

    sw also

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fed-cache";
+const CACHE_NAME = "FullScreen-Clock-v01";
 this.addEventListener("install", function(event) {
     this.skipWaiting();
     console.log("install service worker");
@@ -12,4 +12,29 @@ this.addEventListener("install", function(event) {
             cache.addAll(cacheResources);
         })
     );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+          return Promise.all(keyList.map(function(key) {
+        if(cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
